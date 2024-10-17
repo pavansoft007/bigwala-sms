75ca33e55abc2bf2ea3abb34f41822f927e090d2
NGamerHere <codingdatta@gmail.com>
2024-10-17 13:00:57 +0530

    added the validation based on the mysql error

--- backend/routes/Students-Adding.js
+++ backend/routes/Students-Adding.js
@@ -53,10 +53,17 @@ StudentsAdding.post('/mobileAPI/add-new-student', async (req, res) => {
         });
     } catch (error) {
         console.error('Error creating student and user:', error);
-        res.status(500).json({
-            message: 'An error occurred while creating student and user',
-            error: error.message
-        });
+        if(error.original.errno === 1062){
+           res.status(403).json({
+               message:error.errors[0].message
+           })
+        }
+        else{
+            res.status(500).json({
+                message: 'An error occurred while creating student and user',
+                error: error.message
+            });
+        }
     }
 });
 

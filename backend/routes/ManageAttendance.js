9b03dc51034bf386f05a9fcec9649cc36c52bc69
NGamerHere <codingdatta@gmail.com>
2024-10-24 22:58:30 +0530

    added the get request for the getting pending request

--- backend/routes/ManageAttendance.js
+++ backend/routes/ManageAttendance.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import studentAttendance from '../models/studentAttendance.js';
 import teacherAttendance from "../models/teacherAttendance.js";
+import Teacher from "../models/Teacher.js";
 import VerifyToken from "../middleware/VerifyToken.js";
 import AdminAuth from "../middleware/AdminAuth.js";
 
@@ -88,6 +89,35 @@ ManageAttendance.post('/mobileAPI/attendance/admin-approve',AdminAuth,async (req
         res.status(500).json({ message: 'Error approving request', error });
     }
 });
+
+ManageAttendance.get('/mobileAPI/attendance/get-pending-request',AdminAuth,async (req, res)=>{
+    try {
+        const  school_id  =req['sessionData']['school_id'];
+        teacherAttendance.belongsTo(Teacher, { foreignKey: 'teacher_id' });
+        const pendingRequests = await teacherAttendance.findAll({
+            where: {
+                school_id: school_id,
+                status: 'pending'
+            },include: [{
+                model: Teacher,
+                attributes: ['teacher_id', 'first_name', 'last_name', 'email', 'phone_number'],
+            }]
+        });
+        console.log(pendingRequests);
+
+        if (pendingRequests.length === 0) {
+            return res.status(404).json({ message: 'No pending attendance requests found' });
+        }
+
+        res.status(200).json(pendingRequests);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error fetching pending requests', error });
+    }
+});
+
+
+
 
 
 

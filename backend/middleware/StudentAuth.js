84e4ea1ce0bdf8e21629257b04468b32d6aea175
NGamerHere <codingdatta@gmail.com>
2024-10-24 22:59:15 +0530

    added the middleware for the student

---
+++ backend/middleware/StudentAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+const StudentAuth = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).json({ message: 'No token provided' });
+
+    try{
+        const bearerToken = token.split(' ')[1];
+        if (!bearerToken) return res.status(403).json({ message: 'No token provided' });
+        const tokenDetails=await jwt.verify(bearerToken,process.env.JWTKEY);
+
+        if(tokenDetails.role=== 'student'){
+            req['sessionData']=tokenDetails;
+            next();
+        }else{
+            res.status(403).json({message:"you dont have access"});
+        }
+
+    }catch (e) {
+        return res.status(500).json({ message: 'Failed to authenticate token' });
+    }
+};
+
+export default StudentAuth;

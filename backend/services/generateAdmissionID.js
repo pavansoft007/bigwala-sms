3bd5a03a3c23b0ca157f4b0ad2da14a380934c5f
NGamerHere <codingdatta@gmail.com>
2024-11-04 23:09:11 +0530

    added the unquine Student which was filter based on the school_id

--- backend/services/generateAdmissionID.js
+++ backend/services/generateAdmissionID.js
@@ -2,16 +2,16 @@ import Student from "../models/Student.js";
 
 async function generateAdmissionID(school_code) {
     const lastStudent = await Student.findOne({
+        where: { school_code },
         order: [['admission_ID', 'DESC']]
     });
 
     let nextAdmissionNumber = 1;
-
     if (lastStudent && lastStudent.admission_ID) {
-        const lastAdmissionNumber = parseInt(lastStudent.admission_ID.substring(3), 10);
+        const lastAdmissionNumber = parseInt(lastStudent.admission_ID.substring(school_code.length), 10);
         nextAdmissionNumber = lastAdmissionNumber + 1;
     }
-
     return `${school_code}${nextAdmissionNumber.toString().padStart(7, '0')}`;
 }
+
 export default generateAdmissionID;

40963033bf78d443707da5ff5d07d08a84dd028d
NGamerHere <codingdatta@gmail.com>
2024-11-23 21:33:19 +0530

    removed the old otp verifications

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -36,121 +36,7 @@ Auth.post('/mobileAPI/otp-request', async (req, res) => {
    res.status(200).json({ message: 'OTP sent successfully', token: token });
 });
 
-Auth.post('/mobileAPI/otp-verify', otpTokenVerification ,async (req, res) => {
-   const { otp } = req.body;
-   const token=req['tokenDetails'];
-   if (!otp || !token) {
-      return res.status(400).json({ message: 'OTP and token are required' });
-   }
-   const otpPattern = /^[0-9]{6}$/;
-   if (!otpPattern.test(otp)) {
-      return res.status(400).json({ message: 'plz enter proper six digit OTP' });
-   }
-
-   try {
-      const decoded = jwt.verify(token, JWT_SECRET);
-      if (decoded.otp === otp || otp === "123456") {
-
-         const user = await User.findOne({ where: { phone_number: decoded.phone } });
-
-         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-         }
-
-
-         if (user.role === 'student') {
-            Classroom.hasMany(Student, { foreignKey: 'assginedClassroom' });
-            Student.belongsTo(Classroom, { foreignKey: 'assginedClassroom' });
-            const student = await Student.findOne({
-               where: {
-                  phone_number: user.phone_number
-               },
-               include: [
-                  {
-                     model: Classroom,
-                     required: true
-                  }
-               ]
-            });
-            console.log(student.school_id);
-            const studentData={
-               student_id: student.student_id,
-               first_name: student.first_name,
-               last_name: student.last_name,
-               date_of_birth: student.date_of_birth,
-               gender: student.gender,
-               email: student.email,
-               phone_number: student.phone_number,
-               address: student.address,
-               enrollment_date: student.enrollment_date,
-               status: student.status,
-               school_id:student.school_id,
-               standard:student.Classroom.standard,
-               section:student.Classroom.section,
-               role:"student"
-            };
-            const token = await jwt.sign(
-                studentData,
-                JWT_SECRET,
-                { expiresIn: '360m' }
-            );
-
-
-            if (student) {
-               return res.status(200).json({
-                  message: 'OTP verified successfully',
-                  role:"student",
-                  token:token,
-                  student: studentData
-               });
-            } else {
-               return res.status(404).json({ message: 'Student not found' });
-            }
-         } else if (user.role === 'teacher' || user.role==='admin-teacher'){
-            const teacher = await Teacher.findOne({ where: { phone_number: user.phone_number } });
-            const teacherDetails={
-               teacher_id: teacher.teacher_id,
-               first_name: teacher.first_name,
-               last_name: teacher.last_name,
-               email: teacher.email,
-               phone_number: teacher.phone_number,
-               subject_specialization: teacher.subject_specialization,
-               hire_date: teacher.hire_date,
-               status: teacher.status,
-               school_id:teacher.school_id,
-               assignedClass:teacher.assignedClass,
-               role: teacher.adminAccess ? "admin-teacher" : "teacher"
-            };
-            const token=await jwt.sign(
-                teacherDetails,
-                JWT_SECRET,
-                {expiresIn: '360m'}
-            )
-
-            if (teacher) {
-               return res.status(200).json({
-                  role:teacher.adminAccess ? "admin-teacher" : "teacher",
-                  token:token,
-                  teacher: teacherDetails
-               });
-            } else {
-               return res.status(404).json({ message: 'Teacher not found' });
-            }
-         }
-         else {
-            return res.status(400).json({ message: 'Invalid role' });
-         }
-
-      } else {
-         return res.status(400).json({ message: 'Invalid OTP' });
-      }
-   } catch (error) {
-      console.log(error);
-      return res.status(400).json({ message: 'Invalid or expired token' });
-   }
-});
-
-Auth.post('/mobileAPI/new-otp-verify', otpTokenVerification, async (req, res) => {
+Auth.post('/mobileAPI/otp-verify', otpTokenVerification, async (req, res) => {
    const { otp } = req.body;
    const token = req['tokenDetails'];
 
@@ -189,7 +75,6 @@ Auth.post('/mobileAPI/new-otp-verify', otpTokenVerification, async (req, res) =>
             if (!student) {
                return res.status(404).json({ message: 'Student not found' });
             }
-            console.log(student.admission_ID);
 
             const studentData = {
                student_id: student.student_id,

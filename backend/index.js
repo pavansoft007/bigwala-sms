5c387a790877c8c33c9794db01d8d9abe4d293b5
NGamerHere <codingdatta@gmail.com>
2025-01-11 08:30:43 +0530

    added the form parser

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ import ManagingFeeCategory from "./routes/ManagingFeeCategory.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
     origin: '*',

5231f4f80cb26222485be72e02dc4c26ec514a31
NGamerHere <codingdatta@gmail.com>
2025-03-02 15:41:03 +0530

    added the feature for the all collecting the details of the new intersted schools

---
+++ frontend/src/pages/dashboard/fee/FeeMainPage.tsx
@@ -0,0 +1,12 @@
+const FeeMainPage=()=>{
+    return <div className="m-2 border bg-white rounded-md m-3" >
+        <div>
+            <h1 className="text-2xl rounded-md" >Fee Management Page</h1>
+        </div>
+
+
+
+
+    </div>
+}
+export default FeeMainPage;
